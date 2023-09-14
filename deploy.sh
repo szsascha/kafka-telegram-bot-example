3dee9a7777fc98ad3bbb50ecb9b2cbdb1215beea
szsascha <43013380+szsascha@users.noreply.github.com>
2023-09-14 18:50:59 +0200

    Rename .env file and make TELEGRAM_BOT_API_KEY mandatory

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "TELEGRAM_BOT_API_KEY=$TELEGRAM_BOT_API_KEY" > .env
+
 docker compose build
 
 for img in $(docker-compose config | awk '{if ($1 == "image:") print $2;}'); do
@@ -11,5 +13,4 @@ echo $images
 docker image save $images
 docker-compose -p "kafka-telegram-bot-example" -H "$DOCKER_REMOTE_HOST" down --rmi all
 docker -H "$DOCKER_REMOTE_HOST" image load
-echo "TELEGRAM_BOT_API_KEY=$TELEGRAM_BOT_API_KEY" > .actions_env
-docker-compose -p "kafka-telegram-bot-example" -H "$DOCKER_REMOTE_HOST" --env-file ./.actions_env up --force-recreate -d
+docker-compose -p "kafka-telegram-bot-example" -H "$DOCKER_REMOTE_HOST" up --force-recreate -d
